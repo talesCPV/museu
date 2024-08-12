bb908ed144d0af89e5d2014f68723102d4ef5a94
talesCPV <tales@flexibus.com.br>
2024-08-12 16:38:34 -0300

    by_script

--- CDN/js/acervodigital.js
+++ CDN/js/acervodigital.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost/museu/API/'
+const _URL_ = 'http://localhost/museu/'
 
 
 function getAcervo(token,id_div=''){
@@ -6,7 +6,7 @@ function getAcervo(token,id_div=''){
     const data = new URLSearchParams()
         data.append("token", token)
 
-    const myRequest = new Request(API_URL+"getItens.php",{
+    const myRequest = new Request(_URL_+"API/getItens.php",{
         method : "POST",
         body : data
     });
@@ -27,46 +27,92 @@ function getAcervo(token,id_div=''){
     }else{
         out.then((txt)=>{
             const json = JSON.parse(txt)
-console.log(json)                        
+             
             const main_div = document.getElementById(id_div)
             main_div.classList.add('main-content')
-            
-            function addLine(label,value){
-                const line = document.createElement('div')
-                line.className = 'row-item'
-                const lbl = document.createElement('label')
-                lbl.innerHTML = label
-                line.appendChild(lbl)
-                const vl = document.createElement('p')
-                vl.innerHTML = value
-                line.appendChild(vl)
+            fetch(_URL_+'CDN/microdata/veiculo.json')
+            .then((response)=>{
+                response.text(txt).then((txt)=>{
+                    const microdata = JSON.parse(txt)
 
-                return line
-            }
+                    function addLine(obj,key,label=''){
+                        label = label=='' ? key : label
+                        const line = document.createElement('div')
+                        line.className = 'row-item'
+                        line.innerHTML = `<label>${label}</label><p ${microdata.hasOwnProperty(key) ? `itemprop="${microdata[key]}"` : ''}>${obj[key]}</p>`
+                        return line
+                    }
 
-            for(let i=0; i<json.length; i++){
+                    function addFiles(data){
+                        const div = document.createElement('div')
+                        div.className = 'img-board'
+                        for(let i=0; i<data.files.length; i++){
 
-                const content_div = document.createElement('div')
-                content_div.className = 'content-item'
-    
-/*
-                const div = document.createElement('div')
-                const nome = document.createElement('div')
-                nome.innerHTML = `<p>Acervo:</p><p id="nome-acervo">${json[i].acervo}</p>
-                                <img src="${json[i].files[0]}"/>`
-            
-                div.appendChild(nome)
-*/    
-                content_div.appendChild(addLine('Nome:',json[i].nome))
-                content_div.appendChild(addLine('Descrição:',json[i].obs))
-                content_div.appendChild(addLine('Ano::',json[i].ano))
-                content_div.appendChild(addLine('Chassi:',json[i].chassi))
-                content_div.appendChild(addLine('Marca:',json[i].marca))
+                            const ext = data.files[i].split('.')[data.files[i].split('.').length -1]
+                            const filename = data.files[i].split('.')[0]
+                            const arq = ['jpg','jpeg','png'].includes(ext) ? 'img' : 'div'
 
-                main_div.appendChild(content_div)
-            }
+                            const sqr = document.createElement('div')
+                            sqr.className = 'square-item'
+                            sqr.src = data.files[i]
 
-            
+
+                            sqr.innerHTML = ['jpg','jpeg','png'].includes(ext) ? `<img class="img-item" src="${data.files[i]}">` : `<p>${filename}</p><h1>${ext.toUpperCase()}</h1>`
+
+                            div.appendChild(sqr)
+                        }
+                        return div
+                    }
+
+
+                    for(let i=0; i<json.length; i++){
+//console.log(json[i])
+
+                        const imager = addFiles(json[i])
+console.log(imager.querySelectorAll('.square-item').length )
+                        const content_div = document.createElement('div')
+                        content_div.className = 'content-item'                    
+                        content_div.setAttribute("itemprop", "itemOffered")
+                        content_div.setAttribute("itemscope",true)
+                        content_div.setAttribute("itemtype", "https://schema.org/Car")
+
+
+                        
+                        content_div.appendChild(imager)
+
+                        content_div.appendChild(addLine(json[i],'acervo','Acervo:'))
+                        content_div.appendChild(addLine(json[i],'alt','Altura:'))
+                        content_div.appendChild(addLine(json[i],'ano','Ano:'))
+                        content_div.appendChild(addLine(json[i],'cat','Categoria:'))
+                        content_div.appendChild(addLine(json[i],'chassi','Chassi:'))
+                        content_div.appendChild(addLine(json[i],'cilindrada','Cilindradas:'))
+                        content_div.appendChild(addLine(json[i],'cilindros','Cilindros:'))
+                        content_div.appendChild(addLine(json[i],'comp','Comprimento:'))
+                        content_div.appendChild(addLine(json[i],'cor','Cor:'))
+                        content_div.appendChild(addLine(json[i],'entre_eixo','Entre Eixo:'))
+                        content_div.appendChild(addLine(json[i],'id'))
+                        content_div.appendChild(addLine(json[i],'id_acervo'))
+                        content_div.appendChild(addLine(json[i],'id_item'))
+                        content_div.appendChild(addLine(json[i],'id_veiculo'))
+                        content_div.appendChild(addLine(json[i],'larg','Largura:'))
+                        content_div.appendChild(addLine(json[i],'lugares','Lugares:'))
+                        content_div.appendChild(addLine(json[i],'marca','Marca:'))
+                        content_div.appendChild(addLine(json[i],'modelo','Modelo:'))
+                        content_div.appendChild(addLine(json[i],'nome','Nome:'))
+                        content_div.appendChild(addLine(json[i],'obs','Obs:'))
+                        content_div.appendChild(addLine(json[i],'pais','País:'))
+                        content_div.appendChild(addLine(json[i],'placa','Placa:'))
+                        content_div.appendChild(addLine(json[i],'portas','Portas:'))
+                        content_div.appendChild(addLine(json[i],'pot_hp','Potência HP:'))
+                        content_div.appendChild(addLine(json[i],'tipo','Tipo:'))
+                        content_div.appendChild(addLine(json[i],'vel_max','Velocidade Máxima:'))
+
+                        main_div.appendChild(content_div)
+                    }
+
+
+                })
+            })            
         })
     }
 }
